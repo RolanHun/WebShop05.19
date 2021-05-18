7f3d3181ea81403e649a8a2a15d7891dec6261a5
RolanHun <kisbartok01@gmail.com>
2021-05-19 00:22:27 +0200

    Feladat: 0,1,2,3,4,5,6

--- public_html/JSWeb.js
+++ public_html/JSWeb.js
@@ -1,45 +1,16 @@
 $(function(){
     for (var i = 0; i < targyak.length; i++) {
-        $("#alap").append(targyak[i].nev+" | "+targyak[i].ar+" | "+targyak[i].evjarat+"<br>");
-        $("#alap").append("<br>");
-        $("#alap").append("");
+        $("#alap").append("<tr><td>"+targyak[i].nev+"</td><td>"+targyak[i].ar+"</td><td>"+targyak[i].evjarat+"</td></tr>");
     }
     $("div img").click(kattintasra);
     $("#ok").click(ujTermek);
 });
 
-var targy1={
-    nev:"Audi R8",
-    ar:"60 000 000 Ft",
-    evjarat:"2021",
-    id:"1"
-};
-var targy2={
-    nev:"Lamborghini Huracan",
-    ar:"100 000 000 Ft",
-    evjarat:"2020",
-    id:"2"
-};
-var targy3={
-    nev:"BMW M4",
-    ar:"20 000 000 Ft",
-    evjarat:"2016",
-    id:"3"
-};
-var targy4={
-    nev:"Ferrari 488",
-    ar:"80 000 000 Ft",
-    evjarat:"2019",
-    id:"4"
-};
-var targy5={
-    nev:"Mercedes C63S",
-    ar:"40 000 000 Ft",
-    evjarat:"2021",
-    id:"5"
-};
-
-
+var targy1 = JSON.parse('{ "nev":"Audi R8", "ar":"60 000 000 Ft", "evjarat":"2021"}');
+var targy2 = JSON.parse('{ "nev":"Lamborghini Huracan", "ar":"100 000 000 Ft", "evjarat":"2020"}');
+var targy3 = JSON.parse('{ "nev":"BMW M4", "ar":"20 000 000 Ft", "evjarat":"2016"}');
+var targy4 = JSON.parse('{ "nev":"Ferrari 488", "ar":"120 000 000 Ft", "evjarat":"2020"}');
+var targy5 = JSON.parse('{ "nev":"Mercedes C63S", "ar":"40 000 000 Ft", "evjarat":"2021"}');
 
 var targyak=[targy1,targy2,targy3,targy4,targy5];
 
@@ -48,8 +19,7 @@ function ujTermek() {
             ujTargy.nev=$("#NewNev").val();
             ujTargy.ar=$("#NewAr").val();
             ujTargy.evjarat=$("#NewEvjarat").val();
-        $("#alap").append(ujTargy.nev+" | "+ujTargy.ar+" | "+ujTargy.evjarat+"<br>");
-        $("#alap").append("<br>");
+        $("#alap").append("<tr><td>"+ujTargy.nev+"</td><td>"+ujTargy.ar+"</td><td>"+ujTargy.evjarat+"</td></tr>");
         
 };
 
